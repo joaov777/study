aba91773fe6081c2061d525833cfa7f5a09ca746
Joao <39555841+joaov777@users.noreply.github.com>
2021-06-19 10:24:51 -0300

    Update basic.py

--- Python/programs/basic.py
+++ Python/programs/basic.py
@@ -37,3 +37,44 @@ num2 = float(input("Enter the second number: "))
 check_operator(op,num1,num2)
 """
 
+import sys
+
+# Receives the command line parameter and uses function 
+# check_age to process the information by returning a 
+# message. 
+#
+# Python sys module provides access to any command-line
+# arguments via the sys.argv. 
+#
+# sys.argv[0] ==> script name
+# sys.argv[1] ==> script first command-line parameter
+
+
+def check_age(age):
+    if age < 18:
+        print("Você não tem idade para entrar na festa!")
+    elif age >=65:
+        print("Você tem acompanhante?")
+    else:
+        print("Bem vindo(a)!")
+
+check_age(int(sys.argv[1]))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
