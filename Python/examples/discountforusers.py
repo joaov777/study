98bdd6608514d69af477e75ee9a2f4e13bc142de
Joao <39555841+joaov777@users.noreply.github.com>
2021-11-17 10:13:51 -0300

    Update discountforusers.py

--- Python/examples/discountforusers.py
+++ Python/examples/discountforusers.py
@@ -3,20 +3,22 @@
 #Applying discount based on the type of user.
 
 # base price for ticket 
-TICKET_PRICE = 193.50
+TICKET_PRICE = 1000
 
-# discount applied for VIP clients
-DISCOUNT_FOR_VIP = 0.5
+# discount constant
+DISCOUNT = 0.0
 
 # calculate final ticket price for client (either Standard or VIP)
 def calculateFinalPrice(clientName, clientType):
-    global TICKET_PRICE, DISCOUNT_FOR_VIP
+
+    #declaring global variable in the local scope
+    global TICKET_PRICE, DISCOUNT
     
-    if clientType.lower() == "s":
-        DISCOUNT_FOR_VIP = 0.0    
+    #calculating discount rate
+    DISCOUNT = 0.1 if clientType.lower() == "s" else 0.5  
 
-    return f"Final price for {clientName} is R${TICKET_PRICE - (TICKET_PRICE * DISCOUNT_FOR_VIP):,.2f}"
-
+    #returning final price
+    return f"Final price for {clientName} is R${TICKET_PRICE - (TICKET_PRICE * DISCOUNT):,.2f}"
 
 # parameters
 clientNameArgument = input("Name: ")
