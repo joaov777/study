9165fafb1dfd6b11be5e80f2b007eb00b30b704a
joaov777 <joaov777@gmail.com>
2022-01-30 17:52:03 -0300

    Adding a little more knowledge to oop3.py

--- Python/oop/oop3.py
+++ Python/oop/oop3.py
@@ -1,5 +1,4 @@
-
-
+import random
 
 class Item:
 
@@ -25,8 +24,27 @@ class Item:
         # actions to execute by default
         Item.all_items.append(self)
 
+    # this is a regular method. It takes the "self" (instance) as its parameter
     def calculate_total_price(self):
         return self.price * self.quantity
+
+    # this is a class method. It takes the "cls" (class) as its parameter
+    # the chagne here affects the class attribute and consequently all instances accessing the class attributes (shared among all objects)
+    @classmethod
+    def change_pay_rate(cls, new_pay_rate):
+        cls.pay_rate = new_pay_rate 
+
+    # static method --> it takes nothing as a parameter (no cls or self)
+    # looks like a normal method (function inside a blass) but it acts like a function (functionality outside the class)
+    @staticmethod
+    def greet_person(user_name):
+        print(f"Hello, {user_name}")
+
+    @staticmethod
+    def create_id():
+        random_number = random.randint(10000,19999)
+        return random_number
+        
 
     # method (function inside a class) accessing the class attribute to calculate discount
     def apply_discount(self):
@@ -39,9 +57,10 @@ class Item:
     def __repr__(self):
         return f"Item('{self.name}',{self.price},{self.quantity})"
         
-
-    def get_name(self):
-        return self.name
+    # property decorator creates the getter --> the name of the method must be exactly the same as the attribute
+    @property
+    def name(self):
+        return self._name
 
     def get_price(self):
         return self.price
@@ -49,8 +68,10 @@ class Item:
     def get_quantity(self):
         return self.quantity
 
-    def set_name(self, name):
-        self.name = name
+    # setter decorator creates the setter --> the name of the method must be exactly the same as the attribute
+    @name.setter
+    def name(self, name):
+        self._name = name
 
     def set_price(self, price):
         self.price = price
@@ -89,11 +110,30 @@ print(f"The discount for the {item1.name} was {item1.get_price()}")
 # Laptops have only 30% discount --> the Class attribute can be changed on the fly for the specific instance
 item2.pay_rate = 0.3
 item2.apply_discount()
-print(f"The discount for the {item2.get_name()} was {item2.get_price()}")
+print(f"The discount for the {item2.name} was {item2.get_price()}")
 
 # printing all items (objects) presented on a list
 for instance in Item.all_items:
-    print(instance.get_name())
+    print(instance.name)
+
+item1.name = "Printer"
+
+print(f"The name of the first item will changed to {item1.name}")
+print(f"The pay rate before: {Item.pay_rate}")
+
+# changing the pay rate directly to the class method for this purpose
+# pay_rate is a class attribute and it is shared with all instances 
+Item.change_pay_rate(0.4)
+
+# Accessing the pay rate straight from the class
+print(f"The pay rate now: {Item.pay_rate}")
+
+# Acessing the pay rate through the object
+print(f"The pay rate for {item1.name} is {item1.pay_rate}")
+
+# Static method creating the random ID for the user
+# The static method could have also be accessed by Item.create_id()
+print(f"ID {item1.create_id()} automatically created for {item1.name}")
 
 # printing all objects in a more friendly way
 print(Item.all_items)
