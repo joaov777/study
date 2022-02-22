e993a0dcd0777901e8ecd76af478513634fe863b
joaov777 <joaov777@gmail.com>
2022-02-22 09:23:47 -0300

    Adding youtube.py code improved

--- Python/examples/youtube.py
+++ Python/examples/youtube.py
@@ -1,26 +1,26 @@
 #/usr/bin/env python
 
-# requirements
-# pytube and moviepy --> pip install pytube and pip install moviepy
+# REQUIRED LIBRARIES
+#pip install pytube moviepy ffmpeg
 from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-import os
 from os import system, name
 import time
-import datetime
 from datetime import time, timedelta, datetime
 
 standard_header=">> YOUTUBE VIDEO DOWNLOADER <<"
 
-def header(header):
+#for headers and better UI
+def reloader(header, screen_clear="False"):
+
+    if screen_clear == True:
+        clear_screen()
+
     print(f"{header}")
 
 # clearing the screen 
 def clear_screen():
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
+    system("cls") if name == "nt" else system("clear")
 
 # downloading the video from youtube
 def download_video(video_url, final_video_name):
@@ -36,14 +36,6 @@ def download_video(video_url, final_video_name):
 def cut_video(video_local_path, start_time, end_time, final_file):
 
     print("- Cutting your video...")
-
-    time_format = '%H:%M:%S'
-    #diff = datetime.strptime(end_time, time_format) - datetime.strptime(start_time, time_format)
-
-    #start_time = datetime.datetime(2022,1,1,0,3,10)
-    #end_time = datetime.datetime(2022,1,1,0,6,0)
-    #diff = (end_time-start_time).total_seconds()
-
     ffmpeg_extract_subclip(r"{}".format(video_local_path), time_to_sec(start_time), time_to_sec(end_time), targetname=f"{final_file}.mp4")
 
 # downloading and cutting 
@@ -57,54 +49,50 @@ def time_to_sec(t):
    h, m, s = map(int, t.split(':'))
    return h * 3600 + m * 60 + s
 
+#main menu
 while True:
-    clear_screen()
-
-    header(standard_header)
+    
+    reloader(standard_header)
     print("(1) - Download Youtube video")
     print("(2) - Cut Video")
     print("(3) - Download and cut video")
     user_option = input("Option: ")
 
     match user_option:
-        case ("1"):
+        case "1":
 
             while True:
-
-                clear_screen()
-                header(standard_header)
-                header("> Download Youtube Video")
+                
+                reloader(standard_header, True)
+                reloader("> Download Youtube Video")
             
-                url = input(" - Video URL: ")
+                url = input("- Video URL: ")
                 file_name = input(" - File output name: ")
 
                 if url == "" or file_name == "":
                     print("- Provide the info required!") ; input()
                 else:
-                    if download_video(url,file_name):
-                        print("Video downloaded!") ; input() ; break
-                    else:
-                        print("Sorry, video not downloaded!") ; input() ; break
+                    print("- Success! Video downloaded!") if download_video(url,file_name) else print("- Error! Video not downloaded!")
+                    input()
+                    break
 
-        case ("2"):
+        case "2":
 
-            clear_screen()
-            header(standard_header)
-            header("> Cut video")
+            reloader(standard_header, True)
+            reloader("> Cut video")
             print("Option 2 logic here...") ; input()
 
-        case("3"):
+        case "3":
 
             while True:
 
-                clear_screen()
-                header(standard_header)
-                header("> Download and Cut Youtube video")
+                reloader(standard_header, True)
+                reloader("> Download and Cut Youtube video")
                 
-                url = input(" - Video URL: ")
-                file_name = input(" - File output name: ")
-                start = input(" - Start point: ")
-                end = input(" - End point: ")
+                url = input("- Video URL: ")
+                file_name = input("- File output name: ")
+                start = input("- Start point: ")
+                end = input("- End point: ")
 
                 download_and_cut_video(url, file_name, start, end)
 
