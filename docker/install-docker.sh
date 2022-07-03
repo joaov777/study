6eff47c5d7ac2d2109ec0423bd0ca0b0eab3b5bf
Joao <39555841+joaov777@users.noreply.github.com>
2022-07-03 20:30:14 -0300

    Update install-docker.sh

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-# Installing Docker
+# install docker if not installed
+if [ ! -x "$(command -v docker)" ]; then
+    sudo apt-get install -y apt-transport-https software-properties-common ca-certificates gnupg2
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get update -y
+    sudo apt-get -y install docker-ce
+fi
 
-echo "> Updating system..."
-sudo apt update -y &>/dev/null && sudo apt upgrade -y &>/dev/null &&
-echo "> Installing basic dependencies..." 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common &>/dev/null &&
-echo "> Adding key..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>/dev/null &&
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &>/dev/null &&
-echo "> Updating system..."
-sudo apt update -y &>/dev/null && sudo apt upgrade -y &>/dev/null &&
-echo "> Installing docker..."
-sudo apt install docker-ce &>/dev/null &&
-echo "> Actvating docker daemon..."
-[ ! $(sudo systemctl enable docker --now &>/dev/null) ] || sudo service docker start &>/dev/null &&
+# start the service if not already started
+[ ! $(sudo systemctl enable docker --now &>/dev/null) ] || sudo service docker start
 
-
-# Additional configurations
+# in order not to require sudo for docker commands
 echo "> Creating docker group..."
-sudo groupadd docker &>/dev/null &&
+sudo groupadd docker 
 echo "> Adding $USER to docker group"
-sudo usermod -aG docker $USER &>/dev/null &&
+sudo usermod -aG docker $USER 
 
-
-echo "> Log out and log back in..." &&
-echo "> Test installation with the command: \"docker run -rm hello-world\""
-echo ""
-
-#In case you want to purge docker from your system
-#sudo apt-get purge docker-ce
-#sudo rm -rf /var/lib/docker
-
-#docker run --rm hello-world
+echo "> Log out and log back in..." 
