9fb54d4e7b52b371c98e9edaa6d53c99b6eb4324
joaov777 <joaov777@gmail.com>
2022-07-15 20:24:11 -0300

    updating docker and compose install script

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
+install_docker() {
+
 # install docker if not installed
+echo "--- Installing Docker..." && sleep 1
 if [ ! -x "$(command -v docker)" ]; then
     sudo apt-get install -y apt-transport-https software-properties-common ca-certificates gnupg2
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | tee /etc/apt/sources.list.d/docker.list
-    sudo apt-get update -y
-    sudo apt-get -y install docker-ce
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update -y
+	  sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 fi
 
-# start the service if not already started
-[ ! $(sudo systemctl enable docker --now &>/dev/null) ] || sudo service docker start
+    # start the service if not already started
+    echo "--- Enabling docker service..."
+    sudo systemctl start docker 2> /dev/null || sudo service docker start
 
-# in order not to require sudo for docker commands
-echo "> Creating docker group..."
-sudo groupadd docker 
-echo "> Adding $USER to docker group"
-sudo usermod -aG docker $USER 
+    # in order not to require sudo for docker commands
+    echo "--- Checking docker group..."
+    [ $(getent group docker) ] || groupadd docker && echo "--- Docker group already exists..."
 
-echo "> Log out and log back in..." 
+    echo "--- Adding $USER to docker group"
+    sudo usermod -aG docker $USER
+
+}
+
+apt update -y && apt upgrade -y && apt install sudo curl -y
+echo "--- Preparing to install docker and docker compose..." && sleep 5
+install_docker 
+
+if [ -x "$(command -v docker)" ] && [ -x "$(command -v compose)" ]; then
+    echo "--- Docker installed: $(docker --version)"
+    echo "--- Docker compose installed: $(docker compose version)"
+    echo "--- Log out and log back in..." 
+else 
+    echo "--- ERROR! Either Docker or Compose have not been properly installed!!"
+fi
+
+
+
+
